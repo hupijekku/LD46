11271b8296e8cbd51db27be7a52abdc07a600127
kalehead <7550178+kalehead@users.noreply.github>
2020-04-18 10:43:22 +0300

    Fonts and UI stuff

--- Project/Scripts/ButtonX.gd
+++ Project/Scripts/ButtonX.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var presses = 0
 var maxpresses = 99999
+onready var label = get_node("CanvasLayer/Panel/Label")
 
 func _ready():
-	get_node("CanvasLayer/Panel/MarginContainer/CenterContainer/VBoxContainer/Label").text = "Press the button " + str(maxpresses) + " times!"
+	label.text = "Press the button " + str(maxpresses) + " times!"
 
 func _on_Button_pressed():
 	presses += 1
@@ -13,4 +14,4 @@ func _on_Confirm_pressed():
 	if presses == maxpresses:
 		queue_free()
 	else:
-		get_node("CanvasLayer/Panel/MarginContainer/CenterContainer/VBoxContainer/Label").text = "You screwed up!"
+		label.text = "You screwed up!"
