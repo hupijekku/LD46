546cedb5dd2d5d44015ee0d6c161faec5bf3d1c0
kalehead <7550178+kalehead@users.noreply.github>
2020-04-18 17:48:00 +0300

    Polished ballmaze

--- Project/Scripts/BallMaze.gd
+++ Project/Scripts/BallMaze.gd
@@ -1,10 +1,18 @@
-extends Node2D
+extends CanvasLayer
 
 var prev_mousepos
 var moved
 export var sensitivity = 1000
+var positions = [Vector2(-18,-18), Vector2(18,-18), Vector2(-18,18), Vector2(18,10)]
 
 func _ready():
+	rand_seed(OS.get_time().second)
+	var ballpos = randi() % 4
+	var goalpos = randi() % 4
+	while ballpos == goalpos:
+		goalpos = randi() % 4
+	$BallMaze/Ball.position = positions[ballpos]*4
+	$BallMaze/Maze/Goal.position = positions[goalpos]
 	pass
 	
 func _physics_process(delta):
@@ -12,12 +20,15 @@ func _physics_process(delta):
 		prev_mousepos = get_viewport().get_mouse_position()
 	if Input.is_action_pressed("mouse_left"):
 		moved = get_viewport().get_mouse_position() - prev_mousepos
-#		$Ball.mode = RigidBody2D.MODE_STATIC
-		rotate(max(-0.08, min(moved.x/sensitivity*2*PI, 0.08)))
+		$BallMaze.rotate(-max(-0.08, min(moved.x/sensitivity*2*PI, 0.08)))
 		prev_mousepos = get_viewport().get_mouse_position()
-#	if Input.is_action_just_released("mouse_left"):
-#		$Ball.mode = RigidBody2D.MODE_RIGID
 
-func _input(event):
-	if event is InputEventMouseButton:
-		pass
+
+func _on_Ball_body_entered(body):
+	if body.name == "Goal":
+		body.queue_free()
+		$Panel/Button.disabled = false
+
+
+func _on_Button_pressed():
+	queue_free()
